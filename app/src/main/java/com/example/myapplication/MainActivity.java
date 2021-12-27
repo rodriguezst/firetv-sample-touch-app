b5b349edd0c6c32b6b4ef6b0b661d672dc330dde
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2021-12-27 21:48:07 +0100

    Missing import

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
